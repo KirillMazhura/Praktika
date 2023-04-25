f00b051de625557dd476cc9f7a035cde8c6392dd
KirillMazhura <keremon4ik2005@gmail.com>
2023-04-25 20:50:28 +0200

    added catNdog catalog

--- js/main.js
+++ js/main.js
@@ -1,18 +1,77 @@
-var app = new Vue ({
-    el:"article",
+var app = new Vue({
+    el:".container2",
     data:{
-        products:[{id:1,title:"Prod 1", short_text:'short_text', image: 'TAG10003.png', desc:"Full desc"},
-        {id:2,title:"Prod 2", short_text:'short_text', image: 'TAG10003.png', desc:"Full desc"},
-        {id:3,title:"Prod 3", short_text:'short_text', image: 'TAG10003.png', desc:"Full desc"},
-        {id:4,title:"Prod 4", short_text:'short_text', image: 'TAG10003.png', desc:"Full desc"},
-        {id:5,title:"Prod 5", short_text:'short_text', image: 'TAG10003.png', desc:"Full desc"}]
+        products:[{'id':1, 'title':'TAG 1000 (TAG 853)','image':'image1.jpg', 'short_text': 'Kiwi Gold', 'desc': 'Full description'},
+        {'id':2, 'title':'TAG 1001 (TAG 855)','image':'image2.jpg', 'short_text': 'Kiwi Green', 'desc': 'Full description'},
+        {'id':3, 'title':'TAG 1002 (TAG 809)','image':'image3.jpg', 'short_text': 'Kiwi Green', 'desc': 'Full description'},
+        {'id':4, 'title':'TAG 1003 (TAG 834 )','image':'image4.jpg', 'short_text': 'Actinidia', 'desc': 'Full description'},
+        {'id':5, 'title':'TAG 1004 (TAG 848)','image':'image5.jpg', 'short_text': 'Zespri Red Kiwi', 'desc': 'Full description'}],
+        product:[],
+        cart:[],
+        contactFields:[],
+        menu:0,
+        btnVisible:0,
+        order:0
     },
     mounted:function(){
-        console.log(window.localStorage.getItem('prod'));
+        this.getProduct();
+        this.checkInCart();
+        this.getCart();
     },
     methods:{
-        addItem:function(id){
-            window.localStorage.setItem('prod', id);
-        }
+        getProduct(){
+            if(window.location.hash!=null){
+                let id = window.location.hash.replace('#', '');
+    
+                if(this.products!=null && this.products.length>0){
+                    for(let i in this.products){
+                        if(this.products[i] != null && this.products[i].id != null && this.products[i].id==id) this.product = this.products[i];
+                    }
+                }
+            }
+        },
+        addToCart: function (id) {
+            var cart = [];
+            if (window.localStorage.getItem('cart') != null) {
+                cart = window.localStorage.getItem('cart').split(',');
+            }
+            if (cart.indexOf(String(id)) == -1) {
+                cart.push(id);
+                window.localStorage.setItem('cart', cart.join(','));
+                this.btnVisible = 1;
+            }
+        },
+        checkInCart(){
+            if(this.product!=null && this.product.id!=null && window.localStorage.getItem('cart')!=null && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id))!=-1) this.btnVisible=1;
+        },
+        getCart(){
+            if(window.localStorage.getItem('cart') != null){
+                if(this.products!=null && this.products.length>0){
+                    for(let i in this.products){
+                        if(this.products[i] !=null && this.products[i].id !=null && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id))!=-1)this.cart.push(this.products[i]);
+                    }
+                }
+            }
+        },
+        removeFromCart(id){
+            let cart = [];
+            if(window.localStorage.getItem('cart') != null){
+                cart = window.localStorage.getItem('cart').split(',');
+            }
+
+            if(cart.indexOf(String(id))!=-1){
+                cart.splice(cart.indexOf(String(id)),1);
+                window.localStorage.setItem('cart', cart.join(','));
+                this.cart = [];
+                this.getCart();
+            }
+        },
+        makeOrder(){
+            this.cart = [];
+            window.localStorage.getItem('cart','');
+            this.order=1;
+          }
+        
     }
+
 });
